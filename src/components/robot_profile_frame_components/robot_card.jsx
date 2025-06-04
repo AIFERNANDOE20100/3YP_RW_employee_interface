5e741d09bb211f9b50c62364f5bb4b5e616435ca
kushan malintha <e20243@eng.pdn.ac.lk>
2025-06-04 10:11:52 +0530

    fix css

--- src/components/robot_profile_frame_components/robot_card.jsx
+++ src/components/robot_profile_frame_components/robot_card.jsx
@@ -4,7 +4,7 @@ import "./robot_card.css";
 const RobotCard = ({ name, description, status, imageUrl, onClick }) => {
   return (
     <button className="robot-card" onClick={onClick}>
-      <img src={"./src/assets/robot.jpg"} alt={name} className="robot-image" />
+      <img src={"https://img.freepik.com/free-vector/graident-ai-robot-vectorart_78370-4114.jpg?ga=GA1.1.1902603618.1746611229&semt=ais_hybrid&w=740"} alt={name} className="robot-image" />
       <div className="robot-info">
         <h3>{name}</h3>
         <p>{description}</p>
