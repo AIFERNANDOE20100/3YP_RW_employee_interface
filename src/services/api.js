a32975e606feef2b0ad5eee6a8d68d649dcaf18b
RaviAlpha <ravindupathirage12345@gmail.com>
2025-06-05 21:24:36 +0530

    actual backend ip

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000',
-  // baseURL: 'http://16.176.179.124:5000',
+  // baseURL: 'http://localhost:5000',
+  baseURL: 'http://16.176.179.124:5000',
   headers: {
     'Content-Type': 'application/json',
   },
