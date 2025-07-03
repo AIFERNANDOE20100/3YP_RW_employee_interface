4756ef3a229d228afeef1e344337f3f6860fc600
kushan malintha <e20243@eng.pdn.ac.lk>
2025-07-03 18:36:23 +0530

    make https request

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'http://localhost:5000',
-  baseURL: 'http://16.176.179.124:5000',
+  // baseURL: 'http://16.176.179.124:5000',
+  baseURL: 'https://employee-server.duckdns.org/api',
   headers: {
     'Content-Type': 'application/json',
   },
