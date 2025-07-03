98ca68ffeeb095ff7f1b28ff933bf552a635710c
Akash <e20100@eng.pdn.ac.lk>
2025-07-03 07:47:22 +0530

    just

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,10 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: "http://localhost:5000",
+  baseURL: "http://16.176.179.124:5000",
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
