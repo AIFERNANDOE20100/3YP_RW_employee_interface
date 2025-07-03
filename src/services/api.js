d0ac40bd290ebee5caf5a178c1fa7e4ba3c7fe56
kushan malintha <e20243@eng.pdn.ac.lk>
2025-07-03 19:35:26 +0530

    fix issue

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   // baseURL: 'http://localhost:5000',
   // baseURL: 'http://16.176.179.124:5000',
-  baseURL: 'https://employee-server.duckdns.org/api',
+  baseURL: 'https://employee-server.duckdns.org',
   headers: {
     'Content-Type': 'application/json',
   },
