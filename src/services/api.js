f58a008268afb6a1e2ca2c83eb5e449a372ebe65
kushan malintha <e20243@eng.pdn.ac.lk>
2025-07-11 00:51:52 +0530

    ui/ux

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000',
+  // baseURL: 'http://localhost:5000',
   // baseURL: 'http://16.176.179.124:5000',
-  // baseURL: 'https://employee-server.duckdns.org',
+  baseURL: 'https://employee-server.duckdns.org',
   headers: {
     "Content-Type": "application/json",
   },
